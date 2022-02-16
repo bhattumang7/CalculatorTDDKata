ccab1356c0910df16b46cd9d40d670e8440291af
Bhatt Umang <bhatt.umang7@gmail.com>
2022-02-16 06:33:48 +0530

    Added class to make the test green

---
+++ CalculatorTDDKata/NumberOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTDDKata
+{
+    public class NumberOperation
+    {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
--- TestCalculatorKata/TestNumberOperations.cs
+++ TestCalculatorKata/TestNumberOperations.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CalculatorTDDKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace TestCalculatorKata
