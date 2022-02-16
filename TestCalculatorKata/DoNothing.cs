024f4c819af9cf8b42ee0978aac9166f6fe962bc
Bhatt Umang <bhatt.umang7@gmail.com>
2022-02-16 06:28:19 +0530

    added initial commit

---
+++ TestCalculatorKata/DoNothing.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestCalculatorKata
+{
+    [TestClass]
+    public class DoNothing
+    {
+        [TestMethod]
+        public void ConfirmEnvironmentWokring()
+        {
+            Assert.IsTrue(true, "If this fails, environment is not up");
+        }
+    }
+}
---
+++ TestCalculatorKata/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCalculatorKata")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCalculatorKata")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51f19295-a601-4b07-b72b-3904421e997d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
